eaba19ba1f22a5011bb77c081166f4e837299e78
nhphuc2210 <nhphuc94@gmail.com>
2022-09-30 09:04:42 +0700

    commit

--- cdm/sql/vw__region_qtd_point metrics.sql
+++ cdm/sql/vw__region_qtd_point metrics.sql
@@ -1,4 +1,5 @@
-create or replace view DWH.CDM_DATA.VW__REGION_QTD_POINT as (
+create or replace view DWH.CDM_DATA.VW__REGION_QTD_POINT_METRICS as (
+
 with
 raw_data as (
 select 
@@ -19,31 +20,18 @@ from DWH.CDM_DATA.vw__region__weekly_data
 group by 1,2,3,4,5,6
 )
 
-, point_per_metrics as (
 select a.*
 , ZEROIFNULL(
   case  when AVG_VALUE_REVERSE >= EXCELLENT_CRITERIA_reverse then EXCELLENT_POINTS
         when AVG_VALUE_REVERSE >= GOOD_CRITERIA_reverse then GOOD_POINTS
         else POOR_POINT end
     ) as get_point__quater
+,
+  case  when AVG_VALUE_REVERSE >= EXCELLENT_CRITERIA_reverse then 'Excellent'
+        when AVG_VALUE_REVERSE >= GOOD_CRITERIA_reverse then 'Good'
+        else 'Poor' end
+    as ranking
+, concat(year,quarter, region,metrics_mapping) as _year_quarter_region_metrics
 from raw_data a 
+
 )
-
-, point_of_type as (
-select year, quarter,region
-//, metrics_mapping
-, TYPE
-, "MAX POINT"
-, sum(get_point__quater) as point_of_type
-from point_per_metrics
-group by 1,2,3,4,5
-)
-
-select year, quarter,region, TYPE, "MAX POINT", point_of_type, LEAST(point_of_type, "MAX POINT") as qtd_point
-from point_of_type
-)
-
-
-
-
-
